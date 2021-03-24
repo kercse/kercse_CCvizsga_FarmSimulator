1713135eb708df38b249c7c88bdbb189090d5afb
kercse <69111507+kercse@users.noreply.github.com>
2021-03-24 16:50:53 +0100

    far from finished

--- src/main/java/com/codecool/farm/Farm.java
+++ src/main/java/com/codecool/farm/Farm.java
@@ -1,5 +1,44 @@
 package com.codecool.farm;
 
-class Farm {
+import com.codecool.farm.animal.Animal;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class Farm {
+    List<Animal> animalList;
+
+    public List<Animal> getAnimals() {
+        return animalList;
+    }
+
+    List<String> getStatus() {
+
+        List<String> animalStatus = new ArrayList<>();
+
+        for (int i = 0; i < animalList.size(); i++) {
+            animalStatus.add("There is a " + animalList.get(i).getSize() + " sized "
+                    + animalList.get(i).getSpecies(animalList.get(i)) + "in the farm.");
+        }
+        return animalStatus;
+    }
+
+
+    public Farm(List<Animal> animalList) {
+        this.animalList = animalList;
+    }
+
+    public void feedAnimals() {
+        for (Animal animal : animalList) {
+            animal.feed();
+        }
+    }
+
+    public void butcher(Butcher butcher) {
+        animalList.removeIf(butcher::canButcher);
+    }
+
+    public boolean isEmpty() {
+        return animalList.size() == 0;
+    }
 }
--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -2,4 +2,29 @@ package com.codecool.farm.animal;
 
 public abstract class Animal {
 
+    int size;
+
+    public String getSpecies(Animal animal) {
+        String species;
+        int startingSize = animal.getSize();
+        animal.feed();
+        if (animal.getSize() == startingSize + 2) {
+            species = "cattle ";
+        }
+        species = "pig ";
+        animal.size = startingSize;
+        return species;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Animal() {
+        size = 0;
+    }
+
+    public void feed() {
+
+    }
 }
